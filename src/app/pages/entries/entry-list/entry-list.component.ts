d67dfc0f974f5d73a664e888a07c3174eff54df6
Eduardo Mendonça <eduardo.cecon@gmail.com>
2018-12-14 01:36:54 -0200

    entry list component

---
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,29 @@
+import { Entry } from './../shared/entry.model';
+import { Component, OnInit } from '@angular/core';
+
+import { EntryService } from './../shared/entry.service';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.scss']
+})
+export class EntryListComponent implements OnInit {
+
+  entries: Entry[] = [];
+  constructor(private service: EntryService) { }
+
+  ngOnInit() {
+    this.service.getAll().subscribe(
+      entries => this.entries = entries,
+      () => alert('Error ao carregar lista')
+    );
+  }
+
+  delete(entry) {
+    this.service.delete(entry).subscribe(
+      () => this.entries = this.entries.filter(element => element !== entry),
+      () => alert('Falha excluindo categoria')
+    );
+  }
+}
