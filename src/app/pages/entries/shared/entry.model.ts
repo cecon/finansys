a9d7a5dd08cfc84b19e733d29a39926688e59e2a
Eduardo Mendonça <eduardo.cecon@gmail.com>
2018-12-14 01:03:20 -0200

    Entri add

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,26 @@
+import { Category } from './../../categories/shared/category.model';
+export class Entry {
+    static types = {
+        expense: 'Despesa',
+        renevue: 'Receita'
+    };
+
+    constructor() {
+
+    }
+
+    id?: number;
+    name?: string;
+    description?: string;
+    type?: string;
+    amount?: string;
+    date?: string;
+    paid?: boolean;
+    categoryId?: number;
+    category?: Category;
+
+    get paidText(): string {
+        return this.paid ? 'Pago' : 'Pendente';
+    }
+
+}
